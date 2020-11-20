d817f71738d6cac72d067a92d10a2f201435dcb1
Daan <daan@raow.nl>
2020-11-20 12:11:34 +0100

    bind CacheRepo

---
+++ Repositories/CacheRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OffbeatWP\Cache\Repositories;
+
+class CacheRepository
+{
+
+
+}
--- src/Service.php
+++ src/Service.php
@@ -2,16 +2,21 @@
 
 namespace OffbeatWP\Cache;
 
+use OffbeatWP\Cache\Repositories\CacheRepository;
 use OffbeatWP\Contracts\SiteSettings;
 use OffbeatWP\Services\AbstractService;
 
 class Service extends AbstractService
 {
+
+    public $binding = [
+        CacheRepository::class => CacheRepository::class
+    ];
+
     protected $settings;
 
     public function register(SiteSettings $settings)
     {
-//        test
 
     }
 
