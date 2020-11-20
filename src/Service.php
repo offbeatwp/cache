5a64e662fdcb468d5c9e3752ccd8568b85f7a1e6
Daan <daan@raow.nl>
2020-11-20 11:59:42 +0100

    init

---
+++ src/Service.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OffbeatWP\Cache;
+
+use OffbeatWP\Contracts\SiteSettings;
+use OffbeatWP\Services\AbstractService;
+
+class Service extends AbstractService
+{
+    protected $settings;
+
+    public function register(SiteSettings $settings)
+    {
+        $settings->addPage(AddSettings::class);
+
+    }
+
+}
